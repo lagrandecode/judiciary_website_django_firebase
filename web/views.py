78a495fc1ca3dafdb6e79597b1dd7ba4da4c85f2
lagrandecode <seunogunmolufirst1@gmail.com>
2022-09-26 01:19:12 +0100

    "adding firebase"

--- web/views.py
+++ web/views.py
@@ -6,19 +6,18 @@ from django.shortcuts import render
 #Applyinf firebase to the project 
 # // Your web app's Firebase configuration
 # // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-firebaseConfig = {
-  "apiKey": "AIzaSyDN-Sne0GF1uSNlIYU_aRKqI7iIigAh7ww",
-  "authDomain": "judicary-5d20a.firebaseapp.com",
-  "projectId": "judicary-5d20a",
-  "storageBucket": "judicary-5d20a.appspot.com",
-  "messagingSenderId": "286397750808",
-  "appId": "1:286397750808:web:536792ae3e428ba8d26d6d",
-  "measurementId": "G-BMHM4XDXVY"
-};
+# firebaseConfig = {
+#   "apiKey": "AIzaSyDN-Sne0GF1uSNlIYU_aRKqI7iIigAh7ww",
+#   "authDomain": "judicary-5d20a.firebaseapp.com",
+#   "projectId": "judicary-5d20a",
+#   "storageBucket": "judicary-5d20a.appspot.com",
+#   "messagingSenderId": "286397750808",
+#   "appId": "1:286397750808:web:536792ae3e428ba8d26d6d",
+#   "measurementId": "G-BMHM4XDXVY"
+# };
 
-# // Initialize Firebase
-app = initializeApp(firebaseConfig);
-analytics = getAnalytics(app);
+# # // Initialize Firebase
+# firebase = pyrebase
 
 def home(request):
     return render(request, 'home.html')

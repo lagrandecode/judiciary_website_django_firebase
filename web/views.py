6ee55afdf801f48dea2e0b0b154e05d625e50693
lagrandecode <seunogunmolufirst1@gmail.com>
2022-09-26 01:02:17 +0100

    "commit"

--- web/views.py
+++ web/views.py
@@ -2,5 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+#Applyinf firebase to the project 
+# // Your web app's Firebase configuration
+# // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig = {
+  "apiKey": "AIzaSyDN-Sne0GF1uSNlIYU_aRKqI7iIigAh7ww",
+  "authDomain": "judicary-5d20a.firebaseapp.com",
+  "projectId": "judicary-5d20a",
+  "storageBucket": "judicary-5d20a.appspot.com",
+  "messagingSenderId": "286397750808",
+  "appId": "1:286397750808:web:536792ae3e428ba8d26d6d",
+  "measurementId": "G-BMHM4XDXVY"
+};
+
+# // Initialize Firebase
+app = initializeApp(firebaseConfig);
+analytics = getAnalytics(app);
+
 def home(request):
     return render(request, 'home.html')

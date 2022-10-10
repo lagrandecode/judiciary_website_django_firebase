0ceb08a7f6f8e1ec570b2bedde26145842aea02f
lagrandecode <seunogunmolufirst1@gmail.com>
2022-10-10 18:08:31 +0100

    "commit"

--- judiciary/settings.py
+++ judiciary/settings.py
@@ -50,12 +50,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    
 ]
 
 ROOT_URLCONF = 'judiciary.urls'
@@ -127,6 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
 # Default primary key field type
--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,9 @@
 
 from django.urls import path
 from . import views
+import os
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.home, name='home'),
@@ -10,3 +13,8 @@ urlpatterns = [
     path('map',views.showmap,name='map')
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    

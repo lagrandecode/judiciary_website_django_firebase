aa438e6983a0cea68cbbc5e4608d721ca42f3a5d
lagrandecode <seunogunmolufirst1@gmail.com>
2022-10-10 19:55:59 +0100

    "commit"

--- judiciary/settings.py
+++ judiciary/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+MEDIA_URL = '/images/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 django_heroku.settings(locals())
 

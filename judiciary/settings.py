cda82f07a72f0b89f4ddaee4a1c9f1c4d494ad5f
lagrandecode <seunogunmolufirst1@gmail.com>
2022-10-13 23:33:53 +0100

    "commit"

--- judiciary/settings.py
+++ judiciary/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from pickle import FALSE, TRUE
 import socketserver
 import django_heroku
 import dj_database_url  
@@ -29,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5biqxcrdj7%259$1c@@bo@+%fjci+=&nv2zx59)%9bo265ekes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 

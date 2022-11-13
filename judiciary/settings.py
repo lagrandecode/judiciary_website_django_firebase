ce7fa441a3b69621e2f3475b1d3b9f504c66bb58
lagrandecode <seunogunmolufirst1@gmail.com>
2022-11-13 17:02:35 +0100

    "commit"

--- judiciary/settings.py
+++ judiciary/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR, 'static']
 STATICFILES_DIRS  = (os.path.join(BASE_DIR,'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'

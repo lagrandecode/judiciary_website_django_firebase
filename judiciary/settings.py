f1b36b6f05cfa8d825b0fce8af03d59190fd3eeb
lagrandecode <seunogunmolufirst1@gmail.com>
2022-11-13 20:32:34 +0100

    "commit"

--- judiciary/settings.py
+++ judiciary/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-5biqxcrdj7%259$1c@@bo@+%fjci+=&nv2zx59)%9bo265ekes
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-gunicorn-judiciarywsgi-production.up.railway.app','judiciarytemplate.herokuapp.com']
+ALLOWED_HOSTS = ['web-gunicorn-judiciarywsgi-production.up.railway.app','judiciarytemplate.herokuapp.com','judiciarytemplate.com']
 
 
 # Application definition
